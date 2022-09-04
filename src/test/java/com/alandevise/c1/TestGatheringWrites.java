0f48da3b74cb4e2efbf6915c85ca88208a96337a
Alan Zhang <initiator@alandevise.com>
2022-09-04 13:18:15 +0800

    Just commit.

--- src/test/java/com/alandevise/c1/TestGatheringWrites.java
+++ src/test/java/com/alandevise/c1/TestGatheringWrites.java
@@ -1,9 +1,6 @@
 package com.alandevise.c1;
 
-import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -21,9 +18,9 @@ public class TestGatheringWrites {
     // 汉字在utf8中，占3个字节，所以"你好"占3x2=6 个字节
     ByteBuffer b3 = StandardCharsets.UTF_8.encode("你好");
 
-    try (FileChannel channel = new RandomAccessFile("data.txt", "rw").getChannel()){
-        channel.write(new ByteBuffer[] {b1, b2, b3});
-    }catch(IOException exception){
-        exception.printStackTrace();
-    }
+//    try (FileChannel channel = new RandomAccessFile("data.txt", "rw").getChannel()){
+//        channel.write(new ByteBuffer[] {b1, b2, b3});
+//    }catch(IOException exception){
+//        exception.printStackTrace();
+//    }
 }
