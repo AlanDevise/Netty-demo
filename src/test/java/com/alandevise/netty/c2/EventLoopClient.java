694d8feda74cae3fd6dbbc81781a6e68ac9518fa
Alan <alanbling0120@gmail.com>
2022-10-04 13:55:00 +0800

    Just commit.

---
+++ src/test/java/com/alandevise/netty/c2/EventLoopClient.java
@@ -0,0 +1,47 @@
+package com.alandevise.netty.c2;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @Filename: HelloClient.java
+ * @Package: com.alandevise.netty.c1
+ * @Version: V1.0.0
+ * @Description: 1.
+ * @Author: Alan Zhang [initiator@alandevise.com]
+ * @Date: 2022-10-02 12:11
+ */
+
+public class EventLoopClient {
+    public static void main(String[] args) throws InterruptedException {
+        // 1. 创建启动器
+        Channel channel = new Bootstrap()
+                // 2. 添加EventLoop
+                .group(new NioEventLoopGroup())
+                // 3. 选择客户端的channel实现
+                .channel(NioSocketChannel.class)
+                // 4. 添加处理器
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    // 连接建立后调用
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new StringEncoder(StandardCharsets.UTF_8));
+                    }
+                })
+                // 5. 连接到服务器
+                .connect(new InetSocketAddress("localhost", 8080))
+                // 同步方法（阻塞方法），在连接建立后执行
+                .sync()
+                // 拿到channel连接对象
+                .channel();
+        System.out.println(channel);
+        System.out.println("");
+    }
+}
